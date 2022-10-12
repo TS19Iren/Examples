55feaba7edc8ce1f08d646a9176ef8d4f9652443
«Iren» <guzlik_iris@mail.ru>
2022-10-12 22:08:30 +0300

    commit

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enet user name ");
+string username = Console.ReadLine();
+if(username.ToLower()=="masha")
+{
+    Console.WriteLine("Yee, it is Masha");
+}
+else
+Console.Write("Hello, ");
+Console.WriteLine(username);

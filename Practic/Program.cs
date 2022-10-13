314defb1dfbffc22d8fffb5d0cd63ff5924a8538
«Iren» <guzlik_iris@mail.ru>
2022-10-13 11:37:46 +0300

    commit

---
+++ Practic/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу которая на вход принимает число и выдает его квардрат (число умноженное само на себя)
+Console.Write("Введитте число: ");
+int  number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}"); 
